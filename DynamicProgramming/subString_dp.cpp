ebbcab39bc64103174aa5f26b295d614afeba763
hannahchun <80413027+hannahchun@users.noreply.github.com>
2023-02-22 14:48:10 +0900

    [C++] Dynamic Programming practice2 update1

--- DynamicProgramming/subString_dp.cpp
+++ DynamicProgramming/subString_dp.cpp
@@ -34,36 +34,6 @@ int maxSubStringLength(char *str) {
     }
     
     return maxLen;
-
-    /* dp를 사용하지 않은 풀이 - Brute Force
-    
-    int n = strlen(str);
-    int maxLen = 0;
-
-    // i : 부분 문자열의 시작 인덱스
-    for (int i = 0 ; i < n ; i++) {
-
-        // j : 부분 문자열의 끝 인덱스 (짝수 길이)
-        for (int j = i + 1 ; j < n ; j += 2) {
-            // len : 현재 부분 문자열의 길이
-            int len = j - i + 1;
-
-            // 지금까지의 maxLen가 검사하려는 문자열보다 길면 현재 문자열을 검사하지 않음
-            if (maxLen >= len)
-                continue;
-            
-            int lSum = 0, rSum = 0; 
-            for (int k = 0 ; k < len / 2 ; k++) {
-                lSum += (str[i + k] - '0');
-                rSum += (str[i + k + len / 2] - '0');
-            }
-
-            if (lSum == rSum)
-                maxLen = len;
-        }
-    }
-
-    return maxLen; */
     
 }
 
