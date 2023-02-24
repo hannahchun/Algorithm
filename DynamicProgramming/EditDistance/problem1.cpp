fd4dd929126c7987bc85a452cf75679a2c289cb8
hannahchun <80413027+hannahchun@users.noreply.github.com>
2023-02-24 23:53:46 +0900

    [C++] Dynamic Programming Edit Distance problem1

---
+++ DynamicProgramming/EditDistance/problem1.cpp
@@ -0,0 +1,57 @@
+// Recursion
+#include <iostream>
+
+using namespace std;
+
+int getMinimum(int a, int b, int c) {
+    if (a >= c && b >= c)
+        return c;
+    return a > b ? b : a;
+}
+
+int editDistance(char* str1, char* str2) {
+    // str1이 빈 문자열이면 str2의 모든 글자 삽입
+    if (str1 == NULL || *str1 == '\0')
+        return strlen(str2);
+    // str2가 빈 문자열이면 str1의 모든 글자 삽입
+    if (str2 == NULL || *str2 == '\0')
+        return strlen(str1);
+
+    // 첫 번째 글자가 같으면 첫번째 글자를 무시하고 나머지 단어 간의 최소 교정 비용 구함
+    if(*str1 == *str2)
+        return editDistance(str1 + 1, str2 + 1);
+    
+    // 세가지 연산 후, 최소 교정 비용 구해
+    int d, u, i;
+    // 삭제
+    // 원래 : str1의 첫번째 글자 삭제
+    // 코드 : str1의 첫번째 글자 제외하고, str1과 str2 사이의 최소 교정 비용 구해
+    d = editDistance(str1 + 1, str2);
+    // 치환
+    // 원래 : str1의 첫번째 글자 str2의 첫번째 글자로 치환
+    // 코드 : str1과 str2의 첫번째 글자 제외하고, str1과 str2 사이의 최소 교정 비용 구해
+    u = editDistance(str1 + 1, str2 + 1);
+    // 삽입
+    // 원래 : str2의 첫번째 글자 str1의 제일 앞에 삽입
+    // 코드 : str2의 첫번째 글자 제외하고, str1과 str2 사이의 최소 교정 비용 구해
+    i = editDistance(str1, str2 + 1);
+
+    // 세 연산 이후 최소 교정 비용 간의 최솟값에 1 더하여 반환
+    return getMinimum(d, u, i) + 1;
+}
+
+int main() {
+    char s1[20]; 
+    char s2[20]; 
+
+    cout << "Input word1 : ";
+    cin >> s1;
+
+    cout << "Input word2 : ";
+    cin >> s2;
+
+    cout << "The minimum number of operations required to convert word1 to word2 : " << editDistance(s1, s2);
+    cout << endl;
+
+    return 0;
+}
---
+++ DynamicProgramming/EditDistance/problem1_dp.cpp
@@ -0,0 +1,60 @@
+// dp
+#include <iostream>
+
+using namespace std;
+
+int getMinimum(int a, int b, int c) {
+    if (a >= c && b >= c)
+        return c;
+    return a > b ? b : a;
+}
+
+int editDistance(char* str1, char* str2, int m, int n) {
+    int EditD[m + 1][n + 1];
+
+    // 제일 위 행
+    for (int j = 0 ; j <= n ; j++)
+        EditD[0][j] = j;
+
+    // 제일 왼쪽 열
+    for (int i = 0 ; i <= m ; i++)
+        EditD[i][0] = i;
+    
+    // 나머지
+    for (int i = 1 ; i <= m ; i++) {
+        for (int j = 1 ; j <= n ; j++) {
+
+            // 두 글자가 같다면 
+            if (str1[i - 1] == str2[j - 1])
+                EditD[i][j] = EditD[i - 1][j - 1];
+            
+            // 두 글자가 다르다면
+            else
+                EditD[i][j] = getMinimum(EditD[i][j - 1], EditD[i - 1][j], EditD[i - 1][j - 1]) + 1;
+        }
+    }
+
+    return EditD[m][n];
+}
+
+int main() {
+    char s1[20]; 
+    char s2[20]; 
+    int s1Len;
+    int s2Len;
+
+    cout << "Input word1 : ";
+    cin >> s1;
+
+    s1Len = strlen(s1);
+
+    cout << "Input word2 : ";
+    cin >> s2;
+
+    s2Len = strlen(s2);
+
+    cout << "The minimum number of operations required to convert word1 to word2 : " << editDistance(s1, s2, s1Len, s2Len);
+    cout << endl;
+
+    return 0;
+}
